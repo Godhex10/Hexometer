f290e75281d0ca4943c817d7443aac81dff0c609
Godhex10 <108100749+Godhex10@users.noreply.github.com>
2023-03-20 07:55:38 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+
+# Project Title
+
+**Weatherio** is a Website which gives all the relevant info regarding the Weather
+* It fetches the current location for personalised experience
+* It is *Dynamic* Website which can be used with any device (Mobile,Tablet or PC)
+* With all the current weather details it also displays the weather of the past five days
+
+---
+
+## Tech Stack
+
+**Client:** HTML, CSS, JavaScript, API
+
+---
+
+## Screenshots
+
+![Screenshot 2023-03-17 at 12 47 41 PM](https://user-images.githubusercontent.com/90405823/225838210-acf910f9-d798-444a-a3e4-a35c667085da.jpg)
+
+
+---
+
+## Authors
+
+- [@sham-nt](https://www.github.com/sham-nt)
+
+---
+
---
+++ assets/js/api.js
@@ -0,0 +1,40 @@
+/**
+ * @fileoverview All api related stuff like api_key, api request etc.
+ * @author sham-nt <https://github.com/sham-nt>
+ */
+
+'use strict';
+
+const api_key = "7f93096a67892d31c6a8013b2deb9379";
+
+/**
+ * Fetch data from server
+ * @param {string} URL API url
+ * @param {Function} callback callback
+ */
+export const fetchData = function (URL, callback) {
+  fetch(`${URL}&appid=${api_key}`)
+    .then(res => res.json())
+    .then(data => callback(data));
+}
+
+export const url = {
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+  },
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+  },
+  airPollution(lat, lon) {
+    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+  },
+  reverseGeo(lat, lon) {
+    return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+  },
+  /**
+   * @param {string} query Search query e.g.: "London", "New York"
+   */
+  geo(query) {
+    return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+  }
+}
